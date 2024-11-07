bf488eab4dd72596fb832f44104e561150452431
BENSEGHIRNadjib <nadjib-eddine-adib.benseghir@ens.uvsq.fr>
2024-11-07 19:31:56 +0100

    recieve ip and file in commandline

--- sender.py
+++ sender.py
@@ -6,4 +6,13 @@ def sendy(ip_dest, data, fragment_size=64):
     fragments = [data[i:i+fragment_size] for i in range(0, len(data), fragment_size)]
     
     for i, fragment in enumerate(fragments):
-        packet = IP(dst=)
+        packet = IP(dst=ip_dest) / ICMP(type=8) / fragment
+        send(packet)
+        print(f"pkt {i+1}/{len(fragments)} sent with payload : {fragment}")
+        
+if __name__=="__main__":
+    if len(sys.argv) < 3:
+        print("We use sender.py on --IP DEST-- --FILE--")
+        sys.exit(1)
+    ip_dest = sys.argv[1]
+    file_path = sys.argv[2]
